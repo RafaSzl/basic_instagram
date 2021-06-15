43ac40cf307be86bae7ec26e0176594c389757c8
root <rafalszloda@gmail.com>
2021-06-15 20:39:41 +0000

    adding full ig_clone 7 tables set for future learning

--- INSTAGRAM/ig_clone.sql
+++ INSTAGRAM/ig_clone.sql
@@ -12,3 +12,71 @@ CREATE TABLE users (
 INSERT INTO users (username) VALUES
     ('TOM Brady'),
     ('YOUR mom');
+    
+#example of inserting data into photos table
+INSERT INTO photos (image_url, user_id) VALUES 
+    ('/asdqwe', 1),
+    ('/qweasz', 2);
+    
+#example of join tables photos and users
+SELECT image_url, created_at, username FROM photos LEFT JOIN users ON users.id = photos.user_id;
+
+#example of inserting some comments into comments tables
+INSERT INTO comments (comment_text, user_id, photo_id) VALUES ('habundza', 1, 2), ('Obudz sie maly alibiabo', 2, 3)
+
+#example of changing the column name using alter tables
+ALTER TABLE comments CHANGE comment_id comment_text VARCHAR(255) NOT NULL;
+
+CREATE TABLE photos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+
+);
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
+
+);
+
+CREATE TABLE likes (
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
+CREATE TABLE follows (
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(followee_id, follower_id)
+);
+
+CREATE TABLE tags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR (255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+    photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
+
+
