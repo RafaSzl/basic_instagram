700ccba2af8711311ffc393d0d6b8e0e2d724b46
root <rafalszloda@gmail.com>
2021-06-18 22:43:37 +0000

    updated new_file.sql file with solutions to exercises from udemy sql course

--- INSTAGRAM/new_file.sql
+++ INSTAGRAM/new_file.sql
@@ -1,1 +1,18 @@
-asdadasdasd
+EXERCISE 2
+
+SELECT 
+    id, username, created_at, DAYNAME(created_at) AS name_of_the_day, COUNT(DAYNAME(created_at)) AS count_of_the_day
+FROM users 
+GROUP BY DAYNAME(created_at) ORDER BY count_of_the_day DESC;
+
+
+EXERCISE 3
+
+SELECT * FROM users 
+LEFT JOIN photos 
+ON users.id = photos.user_id
+WHERE photos.id IS NULL;
+
+# PAMIETAJ ZE PRZY NULL NIE MOZESZ DAC ZNAKU = ROWNA SIE BO NIE ZADZIALA
+EXERCISE 4
+
