cbf3c18cd52253af84803722bcb1d20c00d9b186
root <rafalszloda@gmail.com>
2021-06-25 21:52:04 +0000

    whole code that contains 500 randomly generated emails

--- nodejs_sql/app.js
+++ nodejs_sql/app.js
@@ -1,15 +1,56 @@
 var faker = require('faker');
+var mysql = require('mysql');
 
-console.log(faker.internet.email());
-console.log(faker.date.past());
-
-console.log(faker.address.city());
-
-
-
-
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	database: 'join_us'
+});
 
 
+// var q = 'SELECT * FROM users';
 
+// connection.query(q, function (error, results, fields) {
+// 	if (error) throw error;
+// 	console.log(results);
+// });
+
+
+
+// INSERTING DATA IN ANOTHER WAY
+
+// var person = {
+// 	email: faker.internet.email(),
+// 	created_at: faker.date.past()
+// };
+
+// connection.query('INSERT INTO users SET ?', person, function(err, result) {
+// 	if (err) throw err;
+// 	console.log(result);	
+// });
+
+// connection.end(); 
+
+
+// ===================INSERTING LOTS OF DATA IN ANOTHER WAY !!!=================================
+
+
+var data = [];
+for(var i = 0; i < 500; i++){
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+ 
+ 
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+ 
+connection.query(q, [data], function(err, result) {
+  console.log(err);
+  console.log(result);
+});
+ 
+connection.end();
 
 
