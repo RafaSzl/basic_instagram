f627565aea8d58ddce387a6057481affdb3d56d9
root <rafalszloda@gmail.com>
2021-06-27 20:14:23 +0000

    added new folder JoinUs with new app.js file and json files. Also it contains home.ejs file.

---
+++ nodejs_sql/JoinUs/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+
+
+app.set("view engine", "ejs");
+
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	database: 'join_us'
+});
+
+
+app.get("/", function(req, res) {
+// 	FIND COUNT OF USERS IN DATABASE
+	var q = 'SELECT COUNT(*) AS count FROM users';
+	connection.query(q, function(err, results) {
+		if(err) throw err;
+		var count = results[0].count;
+		res.render("home", {count: count});
+	});
+});
+
+app.get("/joke", function(req, res) {
+		res.send("some joke that is made");	
+});
+
+app.get("/rnumber", function(req, res) {
+	var num = Math.floor(Math.random() * 10) + 1;
+	res.send("Some random number is " + num);	
+});
+
+
+app.listen(3000, function(){
+	console.log('Server running on port 3000');
+});
